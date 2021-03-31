564a60c1bfa66299b9d9d16aeaf43453a2c3d01e
victor-arango <73970114+victor-arango@users.noreply.github.com>
2021-03-31 15:20:55 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,42 @@
+const slider = document.querySelector("#slider");
+let sliderSection =document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length-1];
+
+const btnLeft = document.querySelector("#btn--left");
+const btnRight = document.querySelector("#btn--right");
+
+
+slider.insertAdjacentElement('afterbegin',sliderSectionLast);
+
+function Next(){
+    let sliderSectionFirst =document.querySelectorAll(".slider__section")[0];
+    slider.style.marginLeft ="-100%";
+    slider.style.transition ="all 0.5s";
+    setTimeout(function(){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('beforeend',sliderSectionFirst);  
+      },500);
+}
+
+function Prev(){
+    let sliderSection =document.querySelectorAll(".slider__section");
+    slider.style.marginLeft ="0";
+    slider.style.transition ="all 0.5s";
+    setTimeout(function(){
+        slider.style.transition ="none";
+        slider.insertAdjacentElement('afterbegin',sliderSectionLast);  
+      },500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000);
